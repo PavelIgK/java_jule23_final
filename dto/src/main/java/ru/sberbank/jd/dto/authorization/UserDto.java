d94efcd96637f4146bceee0d9d799051892ce4e9
NKostev <nikitos93@mail.ru>
2023-11-14 12:39:35 +0300

    add builders

--- dto/src/main/java/ru/sberbank/jd/dto/authorization/UserDto.java
+++ dto/src/main/java/ru/sberbank/jd/dto/authorization/UserDto.java
@@ -1,11 +1,13 @@
 package ru.sberbank.jd.dto.authorization;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import java.util.UUID;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class UserDto {
 
     private UUID id;
--- dto/src/main/java/ru/sberbank/jd/dto/schedule/ProvidedServiceDto.java
+++ dto/src/main/java/ru/sberbank/jd/dto/schedule/ProvidedServiceDto.java
@@ -1,11 +1,13 @@
 package ru.sberbank.jd.dto.schedule;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import java.util.UUID;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class ProvidedServiceDto {
 
     private UUID id;
