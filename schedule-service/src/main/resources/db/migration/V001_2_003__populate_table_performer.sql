0b2b90eca0dc9690b1850937e04a7907e57085e2
Anton Eltsov <anton-y@yandex.ru>
2023-11-15 22:22:15 +0300

    Добавил скрипт на заполнение клиентов и исполнителей.

---
+++ schedule-service/src/main/resources/db/migration/V001_2_003__populate_table_performer.sql
@@ -0,0 +1,14 @@
+
+insert into schedule_schema.performer(id, user_id, first_name, last_name, phone_number)
+    values(cast('00000000-0000-0000-0000-100000000001' as uuid),
+    cast('00000000-0000-0000-0000-000000000001' as uuid), 'Иван', 'Иванов', '9101111111');
+
+insert into schedule_schema.performer(id, user_id, first_name, last_name, phone_number)
+    values(cast('00000000-0000-0000-0000-200000000002' as uuid),
+    cast('00000000-0000-0000-0000-000000000002' as uuid), 'Петр', 'Петров', '9102222222');
+
+insert into schedule_schema.performer(id, user_id, first_name, last_name, phone_number)
+    values(cast('00000000-0000-0000-0000-200000000003' as uuid),
+    cast('00000000-0000-0000-0000-000000000003' as uuid), 'Сидор', 'Сидоров', '9103333333');
+
+
