69778b0dd522c01360cba08144d22d8cb780e191
Dmitry Kartashov <dmitrykart4shov@gmail.com>
2023-11-16 18:44:31 +0300

    для админки добавлены заказы

---
+++ web-app-service/src/main/java/ru/sberbank/jd/web_app/controller/OrderController.java
@@ -0,0 +1,27 @@
+package ru.sberbank.jd.web_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sberbank.jd.dto.schedule.OrderDto;
+import ru.sberbank.jd.web_app.service.OrderService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public String getAllOrders(Model model) {
+        List<OrderDto> orderDtoList = orderService.findAllOrders();
+        model.addAttribute("orders", orderDtoList);
+        return "orders";
+    }
+}
+
---
+++ web-app-service/src/main/java/ru/sberbank/jd/web_app/service/OrderService.java
@@ -0,0 +1,26 @@
+package ru.sberbank.jd.web_app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import ru.sberbank.jd.dto.schedule.ClientDto;
+import ru.sberbank.jd.dto.schedule.OrderDto;
+import ru.sberbank.jd.web_app.config.WebAppConfig;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final WebAppConfig webAppConfig;
+    private final RestTemplate restTemplate;
+
+    private String getUri(){
+        return "http://" + webAppConfig.getScheduleServiceUrl() + ":" + webAppConfig.getScheduleServicePort() + "/orders";
+    }
+
+    public List<OrderDto> findAllOrders() {
+        return restTemplate.getForObject(getUri(), List.class);
+    }
+}
