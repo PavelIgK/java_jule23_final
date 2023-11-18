89732eeb3dd19cd7c118f2a3d676a28981ab0cf2
NKostev <nikitos93@mail.ru>
2023-11-18 22:20:37 +0300

    fixGoBack

--- bot-app/src/main/java/ru/sberbank/jd/botapp/model/commands/GoBack.java
+++ bot-app/src/main/java/ru/sberbank/jd/botapp/model/commands/GoBack.java
@@ -2,6 +2,9 @@ package ru.sberbank.jd.botapp.model.commands;
 
 import ru.sberbank.jd.botapp.model.ChatInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Команда информация.
  */
@@ -12,9 +15,10 @@ public class GoBack extends AbstractCommandImpl implements Command {
     }
 
     @Override
-    public ChatInfo execute(ChatInfo chatInfo) {
-        chatInfo.getMenuCache().remove(chatInfo.getMenuCache().size()-1);
+    public ChatInfo execute(ChatInfo chatInfo) { //костыль
+        chatInfo.getMenuCache().remove(chatInfo.getMenuCache().size()-1); //удалим последнюю на которой стоим
         AbstractCommandImpl commandToOpen = chatInfo.getMenuCache().get(chatInfo.getMenuCache().size()-1);
+        chatInfo.getMenuCache().remove(chatInfo.getMenuCache().size()-1); //удалим текущую, тк будет дубль
 
         return commandToOpen.execute(chatInfo);
     }
