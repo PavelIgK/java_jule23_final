10e7c5f5789998d1ed79cb559ed2614ce33dc1e2
Sergey Makarov <9206362693@yandex.ru>
2023-11-20 12:15:17 +0300

    fix Service return for DTO List fix Date format for UI

---
+++ bot-app/src/main/java/ru/sberbank/jd/botapp/config/AppContextManager.java
@@ -0,0 +1,19 @@
+package ru.sberbank.jd.botapp.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppContextManager implements ApplicationContextAware {
+    private static ApplicationContext _appCtx;
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx){
+        _appCtx = ctx;
+    }
+
+    public static ApplicationContext getAppContext(){
+        return _appCtx;
+    }
+}
