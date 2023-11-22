7f620fa3ee1e25d30c714a22091d9460a7cfef60
Sergey Makarov <9206362693@yandex.ru>
2023-11-22 12:59:30 +0300

    dynamic date

--- bot-app/src/main/java/ru/sberbank/jd/botapp/model/commands/ChoosePerformer.java
+++ bot-app/src/main/java/ru/sberbank/jd/botapp/model/commands/ChoosePerformer.java
@@ -1,5 +1,9 @@
 package ru.sberbank.jd.botapp.model.commands;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import ru.sberbank.jd.botapp.model.ChatInfo;
 import ru.sberbank.jd.botapp.model.menu.Menu;
@@ -30,31 +34,15 @@ public class ChoosePerformer extends AbstractCommandImpl implements Command {
     @Override
     public ChatInfo execute(ChatInfo chatInfo) {
         commands = new ArrayList<>();
-        commands.add(new ChooseDate("12.11.23"));
-        commands.add(new ChooseDate("13.11.23"));
-        commands.add(new ChooseDate("14.11.23"));
-        commands.add(new ChooseDate("15.11.23"));
-        commands.add(new ChooseDate("16.11.23"));
-        commands.add(new ChooseDate("17.11.23"));
-        commands.add(new ChooseDate("18.11.23"));
-        commands.add(new ChooseDate("19.11.23"));
-        commands.add(new ChooseDate("20.11.23"));
-        commands.add(new ChooseDate("21.11.23"));
-        commands.add(new ChooseDate("22.11.23"));
-        commands.add(new ChooseDate("23.11.23"));
-        commands.add(new ChooseDate("24.11.23"));
-        commands.add(new ChooseDate("25.11.23"));
-        commands.add(new ChooseDate("26.11.23"));
-        commands.add(new ChooseDate("27.11.23"));
-        commands.add(new ChooseDate("28.11.23"));
-        commands.add(new ChooseDate("29.11.23"));
-        commands.add(new ChooseDate("30.11.23"));
-        commands.add(new ChooseDate("01.12.23"));
-        commands.add(new ChooseDate("02.12.23"));
-        commands.add(new ChooseDate("03.12.23"));
-        commands.add(new ChooseDate("04.12.23"));
-        commands.add(new ChooseDate("05.12.23"));
-        commands.add(new ChooseDate("06.12.23"));
+        Date dt = new Date();
+        Calendar c = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat("dd.MM.yy");
+        c.setTime(dt);
+        for (int i = 0; i < 14; i++) {
+            dt = c.getTime();
+            commands.add(new ChooseDate(df.format(dt)));
+            c.add(Calendar.DATE, 1);
+        }
 
         List<AbstractCommandImpl> commandsToView = commands.subList(0, getElemOnPage());
 
