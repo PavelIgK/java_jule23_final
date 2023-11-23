ce3dd42551028c090ef733682c5d7d581a62463c
PavelIgK <pavel.kistenyov@yandex.ru>
2023-11-23 17:01:47 +0300

    feat: удаление устаревших записей из кеша

--- bot-app/src/main/java/ru/sberbank/jd/botapp/repository/UserCacheRepositoryImpl.java
+++ bot-app/src/main/java/ru/sberbank/jd/botapp/repository/UserCacheRepositoryImpl.java
@@ -1,6 +1,13 @@
 package ru.sberbank.jd.botapp.repository;
 
 
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.sberbank.jd.botapp.config.BotConfig;
 import ru.sberbank.jd.botapp.model.UserCache;
 import java.util.*;
 
@@ -8,7 +15,13 @@ import java.util.*;
 /**
  * Реализация хранилища.
  */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+@EnableScheduling
 public class UserCacheRepositoryImpl implements UserCacheRepository {
+
+    private final BotConfig botConfig;
 
     private final Map<Long, UserCache> storage = new HashMap<>();
 
@@ -35,5 +48,25 @@ public class UserCacheRepositoryImpl implements UserCacheRepository {
     @Override
     public boolean contains(Long id) {
         return storage.containsKey(id);
+    }
+
+    /**
+     * Удаляем устаревшие записи.
+     *
+     * @throws InterruptedException
+     */
+    @Scheduled(cron = "${actualize-cache-cron}")
+    public void actualize() throws InterruptedException {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        //Вытащим кого надо удалить
+        List<Long> oldUserCache = storage.entrySet().stream()
+                .filter(it -> it.getValue().getCreateDateTime().plusSeconds(botConfig.getCacheLifetime())
+                        .isBefore(currentDateTime))
+                .map(it -> it.getKey())
+                .toList();
+        //Удаляем из кеша
+        Optional.ofNullable(oldUserCache).orElse(Collections.emptyList())
+                .forEach(storage::remove);
     }
 }
