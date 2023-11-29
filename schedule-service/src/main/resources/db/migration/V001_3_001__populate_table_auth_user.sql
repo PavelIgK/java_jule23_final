54db7362a70f0f893ecc21b34480e46a43ee4453
PavelIgK <pavel.kistenyov@yandex.ru>
2023-11-29 12:18:06 +0300

    refactor: swagger доступ по роли ADMIN

---
+++ schedule-service/src/main/resources/db/migration/V001_3_001__populate_table_auth_user.sql
@@ -0,0 +1,5 @@
+insert into schedule_schema.auth_user(id, enabled, login, password, telegram_id)
+    values(cast('00000000-0000-0000-0000-000000000007' as uuid), true, 'admin', '123', 'adminTG');
+
+
+
---
+++ schedule-service/src/main/resources/db/migration/V001_3_002__populate_table_authority.sql
@@ -0,0 +1,2 @@
+insert into schedule_schema.authority(id, user_id, authority)
+    values(cast('00000000-0000-0000-0000-100000000007' as uuid), cast('00000000-0000-0000-0000-000000000007' as uuid), 'ADMIN');
